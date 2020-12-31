ecbcb27311a413f29ba8b9907eb19ebbf10043e9
vitalikkabai <vitalikkabai@gmail.com>
2020-12-31 19:14:29 +0200

    Updates

---
+++ precache-manifest.0baf18ac9fb10ccc366b6487d1010061.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f68d814f883e3c17c3debbc8dc48766",
+    "url": "/WeatherTrends/index.html"
+  },
+  {
+    "revision": "ef5f726d2316654e5525",
+    "url": "/WeatherTrends/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "28e7404dd71d6afb0881",
+    "url": "/WeatherTrends/static/js/2.a0b79f19.chunk.js"
+  },
+  {
+    "revision": "a51e9d6930fab2afb4bd44dab85916d3",
+    "url": "/WeatherTrends/static/js/2.a0b79f19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef5f726d2316654e5525",
+    "url": "/WeatherTrends/static/js/main.96ea15a4.chunk.js"
+  },
+  {
+    "revision": "a56ed092b2bdbbabccdb",
+    "url": "/WeatherTrends/static/js/runtime-main.5e8a848b.js"
+  }
+]);
